243a0f3a3c93cf83bbb2c40b6a318c2ccdedf4d8
zain-iqbal-3000 <zainiqbal.35201@gmail.com>
2025-05-02 01:45:53 +0500

    home page added

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
     <App />
+    </ThemeProvider>
   </StrictMode>,
 )
---
+++ Frontend/src/theme.jsx
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ffd633',
+      blue: '#040430',
+      red: '#570808',
+    },
+    secondary: {
+      main: '#390099', 
+    },
+  },
+});
+
+export default theme;
