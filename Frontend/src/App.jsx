266ea1e48f366aade99407e0155bde0b44155dc5
zain-iqbal-3000 <zainiqbal.35201@gmail.com>
2025-05-04 03:01:07 +0500

    added admin dashboard

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Home from './Home.jsx'
 import CarDetail from './CarDetail'
+import Dashboard from './Dashboard.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,8 @@ function App() {
   return (
     <>
       {/* <Home /> */}
-      <CarDetail />
+      {/* <CarDetail /> */}
+      <Dashboard />
       
     </>
   )
